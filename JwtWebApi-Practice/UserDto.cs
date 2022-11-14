197138a9b2fcf6d4cd03e841597e5717866f8058
judsonkerller <judsonkerller@gmail.com>
2022-11-14 00:32:09 -0300

    Add project files.

---
+++ JwtWebApi-Practice/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtWebApi_Practice
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
